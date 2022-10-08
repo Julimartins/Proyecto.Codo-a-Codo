3e5893703c4cec349780465ee589acc3d4830c7f
martin <guerreiromartin@gmail.com>
2022-10-08 19:13:31 -0300

    cambios en el form

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,11 @@
 const nav = document.querySelector('.nav');
 const button = document.querySelector('.desplegar');
+const envio_form = document.querySelector('.envio-form')
 
 button.addEventListener('click', ()=>{
     nav.classList.toggle('active')
 })
+
+// envio_form.addEventListener('click', ()=>{
+//     alert('consulta/reserva enviado/a correctamente')
+// })
