485870c39f8c746ee122269fc2bcd273dafd283b
martin <guerreiromartin@gmail.com>
2022-10-08 19:48:20 -0300

    modificaciones form y menu

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ const envio_form = document.querySelector('.envio-form')
 
 button.addEventListener('click', ()=>{
     nav.classList.toggle('active')
+    button.classList.toggle('oscurece')
 })
 
 // envio_form.addEventListener('click', ()=>{
